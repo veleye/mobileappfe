ac31eb7d833aaca842669c78a2328b3edfa60c64
erkan obut <tbd.eobut@softtech.com.tr>
2020-03-27 23:19:14 +0300

    loginform with firebase implemented

--- firstapp/App.js
+++ firstapp/App.js
@@ -1,22 +1,89 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, SwitchComponent } from 'react-native';
 import Header from './src/components/Header'
 import MyList from './src/components/MyList'
-import firebase from 'firebase'
 import LoginForm from './src/components/LoginForm';
+import firebase from '@firebase/app'
+import '@firebase/auth'
+import CardSection from './src/components/CardSection'
+import Spinner from './src/components/Spinner'
+import Button from './src/components/Button'
 
 export default class App extends Component {
 
-  UNSAFE_componentWillMount() {
+  state = {
+    loggedIn : null
   }
 
+  componentWillMount(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyCwMsbPgUCuEjGPOqUClzyZsbPGb5vk66I",
+        authDomain: "mobileappfirebase-de15b.firebaseapp.com",
+        databaseURL: "https://mobileappfirebase-de15b.firebaseio.com",
+        projectId: "mobileappfirebase-de15b",
+        storageBucket: "mobileappfirebase-de15b.appspot.com",
+        messagingSenderId: "909208366514",
+        appId: "1:909208366514:web:e4a636dc5fd81e280ac8ab"
+      };
+      // Initialize Firebase
+      if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
 
-  render(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setState({
+          loggedIn : true
+        });
+      }else{
+        this.setState({
+          loggedIn : false
+        });
+      }
+
+
+    })
+      
+}
+
+renderExitContentPage(){
+  
+  switch(this.state.loggedIn){
+    case true:
+      return(
+        <CardSection>
+          <Button onPress={this.clickLogoutBtn.bind(this)} > 
+              ÇIKIŞ
+          </Button>
+        </CardSection>
+      );
+    case false:
+      return(
+        <LoginForm />
+      );
+    default:
+      return(
+        <View>
+          <Spinner size="large"/>
+        </View>
+      );
+  }
+
+}
+
+clickLogoutBtn(){
+  firebase.auth().signOut();
+}
+
+  render() {
+
+    const { containerStyle } = styles;
+
     return (
     
-      <View style={styles.container}>
+      <View style={containerStyle}>
         <Header headerText={'Giriş Ekranı'} />
-        <LoginForm />
+         {this.renderExitContentPage()}
       </View>
 
     );
@@ -25,9 +92,10 @@ export default class App extends Component {
   
 }
 
-
 const styles = {
-  container: {
+  containerStyle: {
     flex: 1
-  },
+  }
 }
+
+
--- firstapp/src/components/LoginForm.js
+++ firstapp/src/components/LoginForm.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import firebase from '@firebase/app'
 import '@firebase/auth'
-import {View, TextInput} from 'react-native'
+import {Alert, TextInput} from 'react-native'
 import Button from './Button'
 import Card from './Card'
 import CardSection from './CardSection'
+import Spinner from './Spinner'
 
 export default class LoginForm extends Component {
 
@@ -12,40 +13,78 @@ export default class LoginForm extends Component {
         super(props);
         this.state = {
             email:'',
-            password:''
+            password:'',
+            loading:false
         }
     }
 
-    UNSAFE_componentWillMount() {
-        firebase.initializeApp(
-          {
-            apiKey: 'AIzaSyCwMsbPgUCuEjGPOqUClzyZsbPGb5vk66I',
-            authDomain: 'mobileappfirebase-de15b.firebaseapp.com',
-            databaseURL: 'https://mobileappfirebase-de15b.firebaseio.com',
-            projectId: 'mobileappfirebase-de15b',
-            storageBucket: 'mobileappfirebase-de15b.appspot.com',
-            messagingSenderId: '909208366514',
-            appId: '1:909208366514:web:e4a636dc5fd81e280ac8ab'
-          }
-        );
-      }
-
     clickLoginBtn(){
-        let {email, password} = this.state;
-        firebase.auth.signInwithEmailAndPassword(email, password)
-        .then(this.loginSuccess.bind(this))
-        .catch(() => {
-            firebase.auth.createUserWithEmailAndPassword(email, password)
+
+       
+
+        const {email, password} = this.state;
+
+        if(email === '' || password === ''){
+            Alert.alert(
+                'Mesaj',
+                'Kullanıcı adı ve şifre boş bırakılamaz',
+                [{
+                    text:'Tamam', onPress:()=>null
+                }]
+            );
+        }else{
+            firebase.auth().signInWithEmailAndPassword(email, password)
             .then(this.loginSuccess.bind(this))
-            .catch(this.loginFail.bind(this));
-        });
+            .catch(()=>{
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then(this.loginSuccess.bind(this))
+                .catch(this.loginFail.bind(this));
+                
+            });
+
+            this.setState({
+                loading : true
+            });
+        }
+
+        
+    }
+
+    renderContents(){
+        if(!this.state.loading){
+            return(
+                <Button onPress={this.clickLoginBtn.bind(this)} > 
+                        GİRİŞ
+                </Button>
+            );
+        }else{
+            return(
+                <Spinner size = "small"/>
+            );
+        }
     }
 
     loginSuccess(){
+        this.setState({
+            loading : false
+        });
+
         console.log('başarılı');
     }
 
     loginFail(){
+
+        this.setState({
+            loading : false
+        });
+        Alert.alert(
+            'Mesaj',
+            'Kullanıcı adı veya şifre hatalı',
+            [{
+                text:'Tamam', onPress:()=>null
+            }]
+        );
+        
         console.log('hatalı');
     }
 
@@ -65,6 +104,7 @@ export default class LoginForm extends Component {
 
                 <CardSection>
                     <TextInput 
+                        secureTextEntry
                         style={inputStyle} 
                         placeholder="Şifre"
                         value={this.state.password} 
@@ -73,9 +113,7 @@ export default class LoginForm extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <Button onPress={this.clickLoginBtn.bind(this)} > 
-                        GİRİŞ
-                    </Button>
+                    {this.renderContents()}
                 </CardSection>
             </Card>
         )
